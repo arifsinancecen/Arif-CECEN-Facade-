d05c25dda6ac7dbafbaee146efabf0f9621aeea0
Arif Sinan Çeçen <34174457+arifsinancecen@users.noreply.github.com>
2021-11-09 11:04:50 +0300

    Create ComputerFacade.java

---
+++ ComputerFacade.java
@@ -0,0 +1,17 @@
+class ComputerFacade
+{
+    constructor(computer) {
+        this.computer = computer
+    }
+    turnOn() {
+        this.computer.getElectricShock()
+        this.computer.makeSound()
+        this.computer.showLoadingScreen()
+        this.computer.bam()
+    }
+    turnOff() {
+        this.computer.closeEverything()
+        this.computer.pullCurrent()
+        this.computer.sooth()
+    }
+}
